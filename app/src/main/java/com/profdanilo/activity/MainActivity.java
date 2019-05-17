93e72e14a577fe062315137edb25c77545d39b29
Gabriel Oliveira <g-araujo94@hotmail.com>
2019-05-17 08:27:24 -0300

    Add music

--- app/src/main/java/com/profdanilo/activity/MainActivity.java
+++ app/src/main/java/com/profdanilo/activity/MainActivity.java
@@ -27,19 +27,49 @@ public class MainActivity extends AppCompatActivity {
     private int mASoundId;
     private int mBSoundId;
 
+    private SoundPool mSoundPool;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // TODO: Create a new SoundPool
-
+        mSoundPool = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS, AudioManager.STREAM_MUSIC, 0);
 
         // TODO: Load and get the IDs to identify the sounds
-
-
+        mASoundId = mSoundPool.load(getApplicationContext(), R.raw.note1_c,1);
+        mBSoundId = mSoundPool.load(getApplicationContext(), R.raw.note2_d,1);
+        mCSoundId = mSoundPool.load(getApplicationContext(), R.raw.note3_e,1);
+        mDSoundId = mSoundPool.load(getApplicationContext(), R.raw.note4_f,1);
+        mESoundId = mSoundPool.load(getApplicationContext(), R.raw.note5_g,1);
+        mFSoundId = mSoundPool.load(getApplicationContext(), R.raw.note6_a,1);
+        mGSoundId = mSoundPool.load(getApplicationContext(), R.raw.note7_b,1);
     }
 
+    public void playA(View view) {
+        mSoundPool.play(mASoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
+
+    public void playB(View view) {
+        mSoundPool.play(mBSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
+
+    public void playC(View view) {
+        mSoundPool.play(mCSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
+    public void playD(View view) {
+        mSoundPool.play(mDSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
+    public void playE(View view) {
+        mSoundPool.play(mESoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
+    public void playF(View view) {
+        mSoundPool.play(mFSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
+    public void playG(View view) {
+        mSoundPool.play(mGSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+    }
     // TODO: Add the play methods triggered by the buttons
 
 
